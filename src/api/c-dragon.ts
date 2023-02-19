d8c4b7b462bc06949543d87224ea8492dbf3df11
Nathan Hui <hui.n1998@gmail.com>
2023-02-19 18:16:51 +0000

    feat: cdragon api config and utils

---
+++ src/api/c-dragon.ts
@@ -0,0 +1,23 @@
+import { kCDragon, kCDragonCDN, kPatch } from '@/config/constants';
+import { ChampionSummary } from '@/types';
+
+export async function fetchChampionSummaryData() {
+  try {
+    const res = await fetch(`${kCDragon}/v1/champion-summary.json`);
+
+    if (!res.ok) throw new Error(`!res.ok - res.statusText: ${res.statusText}`);
+
+    const data = (await res.json()) as ChampionSummary[];
+
+    if (!data || !data.length) return null;
+
+    return data;
+  } catch (error) {
+    console.log('fetchChampionSummaryData() error:', error);
+    return null;
+  }
+}
+
+export function fetchChampionBanner(championId: number) {
+  return `${kCDragonCDN}/${kPatch}/champion/${championId}/splash-art/centered`;
+}
---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const kPatch = '12.21.1';
+
+export const kCDragon =
+  'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default';
+
+export const kCDragonCDN = `https://cdn.communitydragon.org`;
